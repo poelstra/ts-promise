9d09bd5c8c695110de961e341ad4128dc68c21eb
Martin Poelstra <martin@beryllium.net>
2016-02-25 22:29:59 +0100

    Update to TS 1.8.2, switch to node-style typings resolution.

---
+++ typings/tsd-test.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="./tsd.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="chai/chai.d.ts" />
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,1 @@
 /// <reference path="node/node.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="chai/chai.d.ts" />
