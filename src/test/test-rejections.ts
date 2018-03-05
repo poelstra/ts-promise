7bdb18271bc88e0dc6b5b46f1c4a24bc6a20db71
Martin Poelstra <martin@beryllium.net>
2018-03-05 14:53:17 +0100

    rejections: Add tests, back to 100% coverage.

--- src/test/test-rejections.ts
+++ src/test/test-rejections.ts
@@ -13,10 +13,22 @@ sourceMapSupport.install({
 });
 
 import { expect } from "chai";
-import { UnhandledRejection } from "../lib/rejections";
+import * as sinon from "sinon";
+
+import { Promise } from "../lib/index";
+import { defaultPossiblyUnhandledRejectionHandler, UnhandledRejection } from "../lib/rejections";
 import Trace from "../lib/Trace";
+import { assert } from "../lib/util";
 
 describe("rejections", () => {
+	afterEach(() => {
+		// Back to defaults
+		Promise.flush();
+		Promise.onUnhandledRejection(true);
+		Promise.onPossiblyUnhandledRejection(true);
+		Promise.onPossiblyUnhandledRejectionHandled(true);
+	});
+
 	describe("UnhandledRejection", () => {
 		describe("constructor()", () => {
 			const e = new Error("boom");
@@ -39,6 +51,193 @@ describe("rejections", () => {
 				const ur3 = new UnhandledRejection({}, new Trace());
 				/* tslint:enable:no-unused-variable */
 			});
+		});
+	});
+
+	describe("defaultPossiblyUnhandledHandler", () => {
+		const sandbox = sinon.createSandbox();
+		afterEach(() => {
+			sandbox.restore();
+			// Sandbox doesn't allow to add new properties, so restore won't work :(
+			delete global[<any>"dispatchEvent"];
+			delete global[<any>"PromiseRejectionEvent"];
+		});
+
+		it("should emit event on process in Node and not log if handled", () => {
+			const warnSpy = sandbox.spy(console, "warn");
+			const handlerSpy = sinon.spy();
+			process.on("unhandledRejection", handlerSpy);
+			const reason = new Error("boom");
+			const promise = Promise.reject(reason);
+			Promise.flush();
+			assert(handlerSpy.calledOnce);
+			assert(handlerSpy.calledWithExactly(reason, promise));
+			assert(warnSpy.notCalled);
+			process.removeListener("unhandledRejection", handlerSpy);
+		});
+
+		it("should emit event on process in Node and log if not handled", () => {
+			const warnSpy = sandbox.stub(console, "warn");
+			process.removeAllListeners("unhandledRejection");
+			const emitSpy = sandbox.spy(process, "emit").withArgs("unhandledRejection");
+			const reason = new Error("boom");
+			const promise = Promise.reject(reason);
+			Promise.flush();
+			assert(emitSpy.calledOnce);
+			assert(emitSpy.calledWithExactly("unhandledRejection", reason, promise));
+			assert(warnSpy.calledOnce);
+			assert(/^PossiblyUnhandledRejection: Error: boom/.test(warnSpy.args[0][0]));
+		});
+
+		it("should just warn if process.emit and PromiseRejectionEvent aren't available", () => {
+			const warnSpy = sandbox.stub(console, "warn");
+			sandbox.stub(process, "emit").value(undefined);
+			const reason = new Error("boom");
+			const promise = Promise.reject(reason);
+			Promise.flush();
+			assert(warnSpy.calledOnce);
+			assert(/^PossiblyUnhandledRejection: Error: boom/.test(warnSpy.args[0][0]));
+		});
+
+		it("should emit browser event in non-Node env if PromiseRejectionEvent is available and not warn if handled", () => {
+			const warnSpy = sandbox.stub(console, "warn");
+			sandbox.stub(process, "emit").value(undefined);
+			const sentinel = { foo: "bar" };
+			const preSpy = sinon.spy(() => sentinel);
+			global[<any>"PromiseRejectionEvent"] = preSpy;
+			const dispatchSpy = sinon.spy(() => false); // 'no-one called preventDefault'
+			global[<any>"dispatchEvent"] = dispatchSpy;
+			const reason = new Error("boom");
+			const promise = Promise.reject(reason);
+			Promise.flush();
+			assert(preSpy.calledOnce);
+			assert(preSpy.calledWith("unhandledrejection"));
+			assert(dispatchSpy.calledOnce);
+			assert(dispatchSpy.calledWithExactly(sentinel));
+			assert(warnSpy.notCalled);
+		});
+
+		it("should emit browser event in non-Node env if PromiseRejectionEvent is available and warn if not handled", () => {
+			const warnSpy = sandbox.stub(console, "warn");
+			sandbox.stub(process, "emit").value(undefined);
+			const sentinel = { foo: "bar" };
+			const preSpy = sinon.spy(() => sentinel);
+			global[<any>"PromiseRejectionEvent"] = preSpy;
+			const dispatchSpy = sinon.spy(() => true); // 'someone called preventDefault'
+			global[<any>"dispatchEvent"] = dispatchSpy;
+			const reason = new Error("boom");
+			const promise = Promise.reject(reason);
+			Promise.flush();
+			assert(preSpy.calledOnce);
+			assert(preSpy.calledWith("unhandledrejection"));
+			assert(dispatchSpy.calledOnce);
+			assert(dispatchSpy.calledWithExactly(sentinel));
+			assert(warnSpy.calledOnce);
+			assert(/^PossiblyUnhandledRejection: Error: boom/.test(warnSpy.args[0][0]));
+		});
+
+		it("should not accidentally mark promise as handled", () => {
+			let handled = false;
+			Promise.onPossiblyUnhandledRejection(true);
+			Promise.onPossiblyUnhandledRejectionHandled(() => { handled = true; });
+			const p = Promise.reject(new Error("boom"));
+			sandbox.stub(console, "warn");
+			Promise.flush();
+			expect(handled).to.equal(false);
+		});
+	});
+
+	describe("defaultPossiblyUnhandledRejectionHandledHandler", () => {
+		const sandbox = sinon.createSandbox();
+		beforeEach(() => {
+			Promise.onPossiblyUnhandledRejection(false);
+		});
+		afterEach(() => {
+			sandbox.restore();
+			// Sandbox doesn't allow to add new properties, so restore won't work :(
+			delete global[<any>"dispatchEvent"];
+			delete global[<any>"PromiseRejectionEvent"];
+		});
+
+		it("should emit event on process in Node and not log if handled", () => {
+			const warnSpy = sandbox.spy(console, "warn");
+			const handlerSpy = sinon.spy();
+			process.on("rejectionHandled", handlerSpy);
+			const reason = new Error("boom");
+			const promise = Promise.reject(reason);
+			Promise.flush();
+			promise.suppressUnhandledRejections();
+			Promise.flush();
+			assert(handlerSpy.calledOnce);
+			assert(handlerSpy.calledWithExactly(promise));
+			assert(warnSpy.notCalled);
+			process.removeListener("rejectionHandled", handlerSpy);
+		});
+
+		it("should emit event on process in Node and not log if not handled", () => {
+			const warnSpy = sandbox.stub(console, "warn");
+			process.removeAllListeners("rejectionHandled");
+			const emitSpy = sandbox.spy(process, "emit").withArgs("rejectionHandled");
+			const reason = new Error("boom");
+			const promise = Promise.reject(reason);
+			Promise.flush();
+			promise.suppressUnhandledRejections();
+			Promise.flush();
+			assert(emitSpy.calledOnce);
+			assert(emitSpy.calledWithExactly("rejectionHandled", promise));
+			assert(warnSpy.notCalled);
+		});
+
+		it("should ignore if process.emit and PromiseRejectionEvent aren't available", () => {
+			const warnSpy = sandbox.stub(console, "warn");
+			sandbox.stub(process, "emit").value(undefined);
+			const reason = new Error("boom");
+			const promise = Promise.reject(reason);
+			Promise.flush();
+			promise.suppressUnhandledRejections();
+			Promise.flush();
+			assert(warnSpy.notCalled);
+		});
+
+		it("should emit browser event in non-Node env if PromiseRejectionEvent is available and not warn if handled", () => {
+			const warnSpy = sandbox.stub(console, "warn");
+			sandbox.stub(process, "emit").value(undefined);
+			const sentinel = { foo: "bar" };
+			const preSpy = sinon.spy(() => sentinel);
+			global[<any>"PromiseRejectionEvent"] = preSpy;
+			const dispatchSpy = sinon.spy(() => false); // 'no-one called preventDefault'
+			global[<any>"dispatchEvent"] = dispatchSpy;
+			const reason = new Error("boom");
+			const promise = Promise.reject(reason);
+			Promise.flush();
+			promise.suppressUnhandledRejections();
+			Promise.flush();
+			assert(preSpy.calledOnce);
+			assert(preSpy.calledWith("rejectionhandled"));
+			assert(dispatchSpy.calledOnce);
+			assert(dispatchSpy.calledWithExactly(sentinel));
+			assert(warnSpy.notCalled);
+		});
+
+		it("should emit browser event in non-Node env if PromiseRejectionEvent is available and not warn if not handled",
+				() => {
+			const warnSpy = sandbox.stub(console, "warn");
+			sandbox.stub(process, "emit").value(undefined);
+			const sentinel = { foo: "bar" };
+			const preSpy = sinon.spy(() => sentinel);
+			global[<any>"PromiseRejectionEvent"] = preSpy;
+			const dispatchSpy = sinon.spy(() => true); // 'someone called preventDefault'
+			global[<any>"dispatchEvent"] = dispatchSpy;
+			const reason = new Error("boom");
+			const promise = Promise.reject(reason);
+			Promise.flush();
+			promise.suppressUnhandledRejections();
+			Promise.flush();
+			assert(preSpy.calledOnce);
+			assert(preSpy.calledWith("rejectionhandled"));
+			assert(dispatchSpy.calledOnce);
+			assert(dispatchSpy.calledWithExactly(sentinel));
+			assert(warnSpy.notCalled);
 		});
 	});
 });
